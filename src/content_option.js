805810f64840cf9b8ab3b44047c67888713a2ba6
ckfrontend <cedrickokou91@gmail.com>
2024-12-27 21:40:48 +0100

    Premier commit

---
+++ src/content_option.js
@@ -0,0 +1,110 @@
+import { FaTiktok } from "react-icons/fa";
+
+const logotext = "FIDÈLE-LVDP";
+const meta = {
+    title: "Fidèle-LVDP | Location de Voitures pour Apprendre à Conduire à Paris",
+    description: "Fidèle-LVDP vous propose des véhicules adaptés pour passer votre permis de conduire à Paris. Location flexible et services personnalisés pour les candidats au permis.",
+};
+
+const introdata = {
+    title: "Bienvenue chez Fidèle-LVDP",
+    animated: {
+        first: "Apprenez à conduire avec nous",
+        second: "Réservez votre voiture pour le permis",
+    },
+    description: "Fidèle-LVDP, votre partenaire pour la location de voitures d'apprentissage à Paris. Nous mettons à votre disposition des véhicules sûrs et bien entretenus pour vous accompagner dans votre apprentissage de la conduite.",
+    your_img_url: "https://images.unsplash.com/photo-1591388156010-dd522151da35?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+};
+
+const dataabout = {
+    title: "À propos de Fidèle-LVDP",
+    aboutme: "Fidèle-LVDP est une société spécialisée dans la location de voitures pour l'apprentissage de la conduite à Paris. Nous mettons à votre disposition des véhicules pratiques et bien équipés pour que vous puissiez vous concentrer sur votre formation à la conduite.",
+};
+
+const worktimeline = [
+    {
+        jobtitle: "Location pour apprentissage du permis",
+        where: "Fidèle-LVDP",
+        date: "2024",
+    },
+    {
+        jobtitle: "Véhicules adaptés pour l'apprentissage",
+        where: "Paris",
+        date: "2023",
+    },
+];
+
+const skills = [
+    {
+        name: "Conduite sécuritaire",
+        value: 90,
+    },
+    {
+        name: "Véhicules adaptés",
+        value: 85,
+    },
+    {
+        name: "Facilité de réservation",
+        value: 80,
+    },
+    {
+        name: "Services personnalisés",
+        value: 75,
+    },
+];
+
+const services = [
+    {
+        title: "Location de Voitures pour Permis",
+        description: "Louez une voiture adaptée pour votre apprentissage du permis de conduire. Véhicules bien entretenus et faciles à conduire pour vous aider à réussir.",
+    },
+    {
+        title: "Réservation Flexible",
+        description: "Réservez votre véhicule pour des sessions de conduite à l'heure ou à la journée, selon vos besoins. Des solutions flexibles adaptées à votre emploi du temps.",
+    },
+    {
+        title: "Accompagnement personnalisé",
+        description: "Nous offrons des services supplémentaires pour faciliter votre expérience, y compris la possibilité de réserver des heures de conduite avec un moniteur certifié.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://images.unsplash.com/photo-1570070998935-17658353efb6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+        description: "Solution économique pour l'apprentissage et le passage du permis de conduire.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1614597958478-35ed4ac5c99d?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Véhicules récents pour le confort de nos élèves conducteurs.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "fidele.lvdp.vtc@gmail.com",
+    YOUR_FONE: "+33689422760",
+    description: "Pour toute question contactez-nous à l'adresse ci-dessus. Nous serons ravis de vous accompagner dans votre apprentissage.",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    facebook: "https://www.facebook.com/profile.php?id=61571432900222&mibextid=ZbWKwL",
+    tiktok: "https://www.tiktok.com/@fidele.lvdp.location?lang=fr",
+    whatsapp: "https://chat.whatsapp.com/D11gCCHbOJ4HiGL6yS2kGf"
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
