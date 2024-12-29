dedffadf57fe6d1bbd10e48a8b2371d417eb5fd5
ckfrontend <128625978+ckfrontend@users.noreply.github.com>
2024-12-29 22:13:00 +0100

    Add files via upload

--- src/components/themetoggle/index.js
+++ src/components/themetoggle/index.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { WiMoonAltWaningCrescent4 } from "react-icons/wi";
 
-
 const Themetoggle = () => {
-  const [theme, settheme] = useState(localStorage.getItem("theme"));
+  // Initialise le thème sur 'light' par défaut si rien n'est trouvé dans localStorage
+  const [theme, settheme] = useState(localStorage.getItem("theme") || "light");
+
   const themetoggle = () => {
     settheme(theme === "dark" ? "light" : "dark");
   };
+
   useEffect(() => {
+    // Applique le thème au document
     document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme ); 
+    // Sauvegarde le thème dans localStorage
+    localStorage.setItem('theme', theme);
   }, [theme]);
+
   return (
     <div className="nav_ac" onClick={themetoggle}>
       <WiMoonAltWaningCrescent4 />
